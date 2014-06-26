11334040e5d0dd18c44b72d2966c4daf86145795
dstrekelj <domagojstrekelj@gmail.com>
2014-06-26 12:00:54 +0200

    Revert "Revert "Finished code cleanup""
    This reverts commit fafb2bfd7456a557f528fbdfd52a6602149a8dff.

--- src/gui/TextField.hx
+++ src/gui/TextField.hx
@@ -3,7 +3,8 @@ package gui;
 import com.haxepunk.Entity;
 import com.haxepunk.graphics.Text;
 
-class TextField extends Entity {
+class TextField extends Entity
+{
 	private var textFieldText : Text;
 	private var textFieldOrigin : String;
 	
@@ -15,68 +16,71 @@ class TextField extends Entity {
 						 ?origin : String = "top-left",
 						 ?visible : Bool = true
 						) : Void {
-		super(x, y);
+		super( x, y );
 		
-		textFieldText = new Text(text);
-		textFieldText.size = size;		
+		textFieldText = new Text( text );
+		textFieldText.size = size;
 		textFieldText.align = align;
 		graphic = textFieldText;
 		
 		textFieldOrigin = origin;
-		setTextOrigin(textFieldOrigin);
+		setTextOrigin( textFieldOrigin );
 		
 		type = "textfield";
 		
 		this.visible = visible;
 	}
 	
-	public function setText ( newText : String ) : Void {
+	public function setText ( newText : String ) : Void
+	{
 		textFieldText.text = newText;
-		setTextOrigin(textFieldOrigin);
+		setTextOrigin( textFieldOrigin );
 	}
 	
-	public function setTextOrigin ( newOrigin : String ) : Void {
-		switch (newOrigin) {
+	public function setTextOrigin ( newOrigin : String ) : Void
+	{
+		switch ( newOrigin )
+		{
 		case "top-left":
 			graphic.x = 0;
 			graphic.y = 0;
-			setHitbox(textFieldText.textWidth, textFieldText.textHeight, 0, 0);
+			setHitbox( textFieldText.textWidth, textFieldText.textHeight, 0, 0 );
 		case "top":
-			graphic.x = -textFieldText.textWidth/2;
+			graphic.x = -textFieldText.textWidth / 2;
 			graphic.y = 0;
-			setHitbox(textFieldText.textWidth, textFieldText.textHeight, Std.int(textFieldText.textWidth/2), 0);
+			setHitbox( textFieldText.textWidth, textFieldText.textHeight, Std.int( textFieldText.textWidth / 2 ), 0 );
 		case "top-right":
 			graphic.x = -textFieldText.textWidth;
 			graphic.y = 0;
-			setHitbox(textFieldText.textWidth, textFieldText.textHeight, textFieldText.textWidth, 0);
+			setHitbox( textFieldText.textWidth, textFieldText.textHeight, textFieldText.textWidth, 0 );
 		case "center-left":
 			graphic.x = 0;
-			graphic.y = -textFieldText.textHeight/2;
-			setHitbox(textFieldText.textWidth, textFieldText.textHeight, 0, Std.int(textFieldText.textHeight/2));
+			graphic.y = -textFieldText.textHeight / 2;
+			setHitbox( textFieldText.textWidth, textFieldText.textHeight, 0, Std.int( textFieldText.textHeight / 2 ) );
 		case "center":
-			graphic.x = -textFieldText.textWidth/2;
-			graphic.y = -textFieldText.textHeight/2;
-			setHitbox(textFieldText.textWidth, textFieldText.textHeight, Std.int(textFieldText.textWidth/2), Std.int(textFieldText.textHeight/2));
+			graphic.x = -textFieldText.textWidth / 2;
+			graphic.y = -textFieldText.textHeight / 2;
+			setHitbox( textFieldText.textWidth, textFieldText.textHeight, Std.int( textFieldText.textWidth / 2 ), Std.int( textFieldText.textHeight / 2 ) );
 		case "center-right":
 			graphic.x = -textFieldText.textWidth;
-			graphic.y = -textFieldText.textHeight/2;
-			setHitbox(textFieldText.textWidth, textFieldText.textHeight, textFieldText.textWidth, Std.int(textFieldText.textHeight/2));
+			graphic.y = -textFieldText.textHeight / 2;
+			setHitbox( textFieldText.textWidth, textFieldText.textHeight, textFieldText.textWidth, Std.int( textFieldText.textHeight / 2 ) );
 		case "bottom-left":
 			graphic.x = 0;
 			graphic.y = -textFieldText.textHeight;
-			setHitbox(textFieldText.textWidth, textFieldText.textHeight, 0, textFieldText.textHeight);
+			setHitbox( textFieldText.textWidth, textFieldText.textHeight, 0, textFieldText.textHeight );
 		case "bottom":
-			graphic.x = -textFieldText.textWidth/2;
+			graphic.x = -textFieldText.textWidth / 2;
 			graphic.y = -textFieldText.textHeight;
-			setHitbox(textFieldText.textWidth, textFieldText.textHeight, Std.int(textFieldText.textWidth/2), textFieldText.textHeight);
+			setHitbox( textFieldText.textWidth, textFieldText.textHeight, Std.int( textFieldText.textWidth / 2 ), textFieldText.textHeight );
 		case "bottom-right":
 			graphic.x = -textFieldText.textWidth;
 			graphic.y = -textFieldText.textHeight;
-			setHitbox(textFieldText.textWidth, textFieldText.textHeight, textFieldText.textWidth, textFieldText.textHeight);
+			setHitbox( textFieldText.textWidth, textFieldText.textHeight, textFieldText.textWidth, textFieldText.textHeight );
 		default :
 			graphic.x = 0;
 			graphic.y = 0;
-			setHitboxTo(graphic);
+			setHitboxTo( graphic );
 		}
 	}
 }
